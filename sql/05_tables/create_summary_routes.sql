841e6ca9e2b304ae34c28964387ac5ecd5f82d4d
Oksana Kocherzhat <kocherzhat32@gmail.com>
2026-06-03 18:21:02 +0300

    Create create_summary_routes.sql

---
+++ sql/05_tables/create_summary_routes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `coherent-lock-498314-v2.gtfs_oulu.summary_routes` AS
+SELECT 
+  r.route_short_name,
+  COUNT(t.trip_id) AS trips_count
+FROM `coherent-lock-498314-v2.gtfs_oulu.routes` r
+JOIN `coherent-lock-498314-v2.gtfs_oulu.trips` t 
+  ON r.route_id = t.route_id
+GROUP BY r.route_short_name;
